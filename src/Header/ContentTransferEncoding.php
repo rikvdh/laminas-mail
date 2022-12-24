c96110d0bb93c228df978e88ab4422e8d363a791
Rik van der Heijden <mail@rikvanderheijden.com>
2022-12-24 14:05:22 +0100

    remove trailing semicolons for transferencoding

--- src/Header/ContentTransferEncoding.php
+++ src/Header/ContentTransferEncoding.php
@@ -83,7 +83,7 @@ class ContentTransferEncoding implements HeaderInterface
     public function setTransferEncoding($transferEncoding)
     {
         // Per RFC 1521, the value of the header is not case sensitive
-        $transferEncoding = strtolower($transferEncoding);
+        $transferEncoding = rtrim(strtolower($transferEncoding), ';');
 
         if (! in_array($transferEncoding, static::$allowedTransferEncodings)) {
             throw new Exception\InvalidArgumentException(sprintf(
